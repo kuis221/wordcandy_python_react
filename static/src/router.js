91cd3d18465771c0343eab5e7d515bd001a1e089
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2017-04-05 21:19:33 +0300

    landing

--- static/src/router.js
+++ static/src/router.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import {Router, Route, Link, browserHistory} from 'react-router';
 import Main from './views/main';
 import Dashboard from './views/dashboard';
+import Landing from './views/landing';
 
 function run() {
     ReactDOM.render((
         <Router history={browserHistory}>
             <Route path="/" component={Main}/>
             <Route path="/dashboard" component={Dashboard}/>
+            <Route path="/landing" component={Landing}/>
         </Router>
     ), document.getElementById('app'));
 }

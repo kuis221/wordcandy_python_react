61fb1682807a4f830a4a2fe9e51b192b633a30c9
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2017-06-26 13:36:40 +0300

    improve backend

--- apps/settings.py
+++ apps/settings.py
@@ -220,7 +220,7 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
